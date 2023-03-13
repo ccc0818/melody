f87f0a2c7ebbd24c6315e0aaeeb3f900a0bd66e9
ccc0818 <cc.chn@qq.com>
2023-03-13 15:46:40 +0800

    fix: 修复图片懒加载遇到容器尺寸变化时无法生效的问题

--- src/direactives/vLazyContainer.ts
+++ src/direactives/vLazyContainer.ts
@@ -1,24 +1,26 @@
 import { debounce } from "@/utils";
-
 export default (el: HTMLElement, binding: any) => {
   const lazyNodes = el.querySelectorAll(binding.value.selector);
   if (!lazyNodes.length) {
     return;
   }
 
-  const sizeEl = el.getBoundingClientRect();
+  const update = (e?: Event) => {
+    nextTick(() => {
+      const sizeEl = el.getBoundingClientRect();
 
-  const update = () => {
-    // 延迟加载
-    debounce(() => {
-      lazyNodes.forEach((v: HTMLElement) => {
-        const rect = v.getBoundingClientRect();
-        if (rect.y > sizeEl.y && rect.y < sizeEl.y + sizeEl.height) {
-          // 在可视区域内
-          v.setAttribute("src", v.dataset.src ?? "");
-        }
-      });
-    }, 200);
+      // 延迟加载
+      debounce(() => {
+        lazyNodes.forEach((v: HTMLElement) => {
+          const rect = v.getBoundingClientRect();
+
+          if (rect.y > sizeEl.y && rect.y < sizeEl.y + sizeEl.height) {
+            // 在可视区域内
+            v.setAttribute("src", v.dataset.src ?? "");
+          }
+        });
+      }, 200);
+    })
   };
 
   // 挂载后立即执行一次
